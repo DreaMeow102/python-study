1a295a311f04de2f771517239167d6f2b3d169cb
DreaMeow102 <490000540@qq.com>
2026-09-21 13:09:28 +0800

    更新 2026-09-21 13:09

---
+++ python学习/第五课/code/b1.py
@@ -0,0 +1,5 @@
+num = [10,20,30,40]
+a = 0
+for b in num:
+    a = a + b
+print (a)
---
+++ python学习/第五课/code/b2.py
@@ -0,0 +1,11 @@
+num = [3, 8, 12, 5, 20, 7]
+a = 0
+# for b in num:
+#     if b > 10:
+#         a + 1
+# print (a)
+# 上述为错误逻辑代码，保留错误代码
+for b in num:
+    if b > 10:
+        a = a + 1
+print (a)
